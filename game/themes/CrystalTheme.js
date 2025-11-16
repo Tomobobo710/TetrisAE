d5b6b1c197febeaa8b8f5bce83c1ad019de6b2f6
Tomobobo710 <64335998+Tomobobo710@users.noreply.github.com>
2025-11-16 07:33:11 -0700

    Made crystals more crystal-like in CrystalTheme

--- game/themes/CrystalTheme.js
+++ game/themes/CrystalTheme.js
@@ -346,11 +346,10 @@ class CrystalTheme extends BaseTheme {
             ctx.globalAlpha = 0.2 * opacity * crystalOpacity;
             ctx.fillStyle = gradient;
             ctx.beginPath();
-            ctx.moveTo(0, -40);
-            ctx.lineTo(20, -10);
-            ctx.lineTo(15, 30);
-            ctx.lineTo(-15, 30);
-            ctx.lineTo(-20, -10);
+            ctx.moveTo(0, -45);
+            ctx.lineTo(20, 0);
+            ctx.lineTo(0, 45);
+            ctx.lineTo(-20, 0);
             ctx.closePath();
             ctx.fill();
 
@@ -366,11 +365,10 @@ class CrystalTheme extends BaseTheme {
             ctx.globalAlpha = 0.6 * opacity * crystalOpacity;
             ctx.fillStyle = '#ffffff';
             ctx.beginPath();
-            ctx.moveTo(0, -20);
-            ctx.lineTo(10, -5);
-            ctx.lineTo(8, 15);
-            ctx.lineTo(-8, 15);
-            ctx.lineTo(-10, -5);
+            ctx.moveTo(0, -22.5);
+            ctx.lineTo(10, 0);
+            ctx.lineTo(0, 22.5);
+            ctx.lineTo(-10, 0);
             ctx.closePath();
             ctx.fill();
             
